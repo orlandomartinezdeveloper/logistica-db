3d4a8f5eda5ecdf83e2c799391c190eb61a7b2ce
orlandomartinezdeveloper <omjuniormusic@gmail.com>
2026-08-17 23:49:21 -0300

    actualización de estructura de la base de datos

---
+++ calebito_transporte_db.sql
@@ -0,0 +1,455 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 17-08-2026 a las 23:11:35
+-- Versión del servidor: 5.7.44
+-- Versión de PHP: 8.1.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `calebito_transporte_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `destinations`
+--
+
+CREATE TABLE `destinations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `latitude` decimal(10,7) DEFAULT NULL,
+  `longitude` decimal(10,7) DEFAULT NULL,
+  `type` varchar(50) NOT NULL,
+  `maps_url` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `maintenance_alerts`
+--
+
+CREATE TABLE `maintenance_alerts` (
+  `id` int(11) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `maintenance_item_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `triggered_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `is_resolved` tinyint(1) DEFAULT '0',
+  `resolved_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `maintenance_items`
+--
+
+CREATE TABLE `maintenance_items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `interval_km` int(11) DEFAULT NULL,
+  `interval_months` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `is_read` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `routes`
+--
+
+CREATE TABLE `routes` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `priority` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'em_espera',
+  `store_id` int(11) DEFAULT NULL,
+  `destination_id` int(11) DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `route_assignments`
+--
+
+CREATE TABLE `route_assignments` (
+  `id` int(11) NOT NULL,
+  `route_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `assigned_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `stores`
+--
+
+CREATE TABLE `stores` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL,
+  `maps_url` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `description` text,
+  `priority` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'em_espera',
+  `store_id` int(11) DEFAULT NULL,
+  `destination_id` int(11) DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `task_assignments`
+--
+
+CREATE TABLE `task_assignments` (
+  `id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `role` varchar(20) NOT NULL,
+  `assigned_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `lastname` varchar(150) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(500) NOT NULL,
+  `cnh` varchar(20) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `birth_day` date NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'ativo',
+  `password_hash` varchar(255) NOT NULL,
+  `photo_url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `reset_expires` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `lastname`, `phone`, `address`, `cnh`, `email`, `birth_day`, `role`, `status`, `password_hash`, `photo_url`, `created_at`, `updated_at`, `reset_token`, `reset_expires`) VALUES
+(1, 'Orlando Martinez', '', '22992463411', '', '', 'omjuniormusic@gmail.com', '0000-00-00', 'Motorista', 'ativo', '$2y$10$z.abkzK8/kZ6m7u9ttMrkex9aH.dVDV9HVkymZLEFV0FB5fgAI/qW', NULL, '2026-04-28 17:08:37', '2026-05-25 21:13:27', '4677b98cbd9912465303cf67f0162f2c0a052425711b77e3d886b07323b53fc0', '2026-05-20 01:12:00'),
+(2, 'Michael Exemplo', '', '3423423423', '', '', 'exemplo@exemplo.com', '0000-00-00', 'Motorista', 'ativo', '$2y$10$YViFc6DWL3VlVSh9IvP6f.5m53ze3.RGO9TVRH/ifCSjtDIfCZrUu', NULL, '2026-01-15 03:49:16', '2026-05-25 20:58:47', NULL, NULL),
+(4, 'maria', '', '2299654513', '', '', 'omjuniomusic@gmail.com', '0000-00-00', 'Motorista', 'ativo', '$2y$10$w9nw/c/RlH3DkmMZSHTBieFHEJUlCPvlI7j5ZF58Wa0i9gFcV8z/K', NULL, '2026-05-19 21:41:44', '2026-05-21 22:35:12', 'b5b9cfbf62d3e727d89248fa5189283adebc9f5f852f4460bd2747ea7e6b612e', '2026-05-22 02:35:12'),
+(5, 'sfsdfasd', '', NULL, '', '', 'sdafasd@dsfdf.com', '0000-00-00', 'motorista', 'ativo', '$2y$10$XVRqTVJ3SOb2HR80KpZWLum/ifprPWIV.NMD3FAjhhYOm6youzpEe', NULL, '2026-06-17 21:53:13', '2026-06-17 21:53:13', NULL, NULL),
+(6, 'aaaa', '', NULL, '', '', 'aaa@aaa.com', '0000-00-00', 'gestor_logistica', 'ativo', '$2y$10$QPHqBxfjmwY.E.7xE.9J2Odvw5ic.vLh9zhG3eLGrjTKosoOmBY1q', NULL, '2026-06-17 22:31:27', '2026-06-17 22:31:27', NULL, NULL),
+(7, 'xxxx', '', '21343254534', '', '', 'xxx@xxx.com', '0000-00-00', 'gestor_eventos', 'ativo', '$2y$10$r2KWPopLdMjW9W55QXCTtu.DZNlrE2Q4.w/zJVI/ZKgHaIFkl9Edy', NULL, '2026-06-17 22:36:40', '2026-06-17 22:36:40', NULL, NULL),
+(8, 'aaaaaaaaa', 'bbbbbbbbbb', '32423423', 'fsdfdsafd', 'a', 'asdasdas@asadasd.com', '3333-12-12', 'motorista', 'ativo', '$2y$10$uRHkbvLafw3WT9AEEXXQ0O06M/OXehfRDEkJuEZvust3jRJHRTjUO', '', '2026-08-11 22:37:50', '2026-08-11 22:37:50', NULL, NULL),
+(9, 'xxxxxxxxxx', 'yyyyyyyyyy', '111111111111111', 'dsfsdfsdfsdfsdf', 'a', 'asdasd@sadas.com', '0000-00-00', 'motorista', 'ativo', '$2y$10$1S2Y4OJbG77rHeHUJ/s1oegzIzAc3kvUSnbEKFwjEVUw361uOv6ZG', '', '2026-08-11 22:58:11', '2026-08-11 22:58:11', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id` int(11) NOT NULL,
+  `plate_number` varchar(20) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'ativo',
+  `current_km` bigint(20) DEFAULT '0',
+  `photo_url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehicle_maintenances`
+--
+
+CREATE TABLE `vehicle_maintenances` (
+  `id` int(11) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `maintenance_item_id` int(11) NOT NULL,
+  `performed_at_date` date DEFAULT NULL,
+  `performed_at_km` bigint(20) DEFAULT NULL,
+  `next_due_km` bigint(20) DEFAULT NULL,
+  `next_due_date` date DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehicle_usages`
+--
+
+CREATE TABLE `vehicle_usages` (
+  `id` int(11) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `route_id` int(11) DEFAULT NULL,
+  `start_km` bigint(20) NOT NULL,
+  `end_km` bigint(20) DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `destinations`
+--
+ALTER TABLE `destinations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `maintenance_alerts`
+--
+ALTER TABLE `maintenance_alerts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vehicle_id` (`vehicle_id`),
+  ADD KEY `maintenance_item_id` (`maintenance_item_id`);
+
+--
+-- Indices de la tabla `maintenance_items`
+--
+ALTER TABLE `maintenance_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `routes`
+--
+ALTER TABLE `routes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `store_id` (`store_id`),
+  ADD KEY `destination_id` (`destination_id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- Indices de la tabla `route_assignments`
+--
+ALTER TABLE `route_assignments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `route_id` (`route_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `store_id` (`store_id`),
+  ADD KEY `destination_id` (`destination_id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- Indices de la tabla `task_assignments`
+--
+ALTER TABLE `task_assignments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `plate_number` (`plate_number`);
+
+--
+-- Indices de la tabla `vehicle_maintenances`
+--
+ALTER TABLE `vehicle_maintenances`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vehicle_id` (`vehicle_id`),
+  ADD KEY `maintenance_item_id` (`maintenance_item_id`),
+  ADD KEY `created_by` (`created_by`);
+
+--
+-- Indices de la tabla `vehicle_usages`
+--
+ALTER TABLE `vehicle_usages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vehicle_id` (`vehicle_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `route_id` (`route_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `destinations`
+--
+ALTER TABLE `destinations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `maintenance_alerts`
+--
+ALTER TABLE `maintenance_alerts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `maintenance_items`
+--
+ALTER TABLE `maintenance_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `routes`
+--
+ALTER TABLE `routes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `route_assignments`
+--
+ALTER TABLE `route_assignments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `task_assignments`
+--
+ALTER TABLE `task_assignments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `vehicle_maintenances`
+--
+ALTER TABLE `vehicle_maintenances`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `vehicle_usages`
+--
+ALTER TABLE `vehicle_usages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
